fff3e285dd89b3e42f0b21a2d3048ecb916d8b17
Oleg Rovenskyi <oleg_rovenskyi@epam.com>
2016-03-18 13:35:37 +0200

    init test3 canvas App

---
+++ test3/test3.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Oleg_Rovenskyi on 3/18/2016.
+ */
+var canvas = document.createElement('canvas');
+document.body.appendChild(canvas);
+var context = canvas.getContext('2d');
+
+canvas.width = 500;
+canvas.height = 400;
+
+context.lineWidth = 15;
