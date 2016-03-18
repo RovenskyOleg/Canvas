91d43c3b4d3a3184235b324376996457e8a48755
Oleg Rovenskyi <oleg_rovenskyi@epam.com>
2016-03-18 19:47:01 +0200

    init test5 canvas

--- test4/test4.js
+++ test4/test4.js
@@ -12,8 +12,8 @@ var startAngle = 1.1 * Math.PI;
 var endAngle = 1.9 * Math.PI;
 var counterClockwise = false;
 
-canvas.width = 500;
-canvas.height = 400;
+canvas.width = 1000;
+canvas.height = 800;
 
 context.lineWidth = 5;
 
@@ -21,6 +21,24 @@ context.beginPath();
 
 context.arc(x, y, radius, startAngle, endAngle, counterClockwise);
 
+// line color
+context.strokeStyle = "black";
+context.stroke();
+
+// Квадратичная кривая
+context.beginPath();
+context.moveTo(188, 150);
+context.quadraticCurveTo(288, 0, 388, 150);
+context.lineWidth = 10;
+// line color
+context.strokeStyle = "black";
+context.stroke();
+
+// Кривая Безье
+context.beginPath();
+context.moveTo(188, 130);
+context.bezierCurveTo(140, 10, 388, 10, 388, 170);
+context.lineWidth = 10;
 // line color
 context.strokeStyle = "black";
 context.stroke();
