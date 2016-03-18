d9344713bb700a1793407affa75fe16eb9681c7b
Oleg Rovenskyi <oleg_rovenskyi@epam.com>
2016-03-18 13:31:10 +0200

    add setLineDash canvas App

--- test2/test2.js
+++ test2/test2.js
@@ -10,14 +10,29 @@ canvas.height = 400;
 
 context.lineWidth = 45;
 context.lineCap = 'butt'; //butt round square
-//context.lineJoin = ',iter'; // miter round bevel
-context.miterLimit = 2.1;
-
-//context.setLineDash([10, 20, 5]);
+context.lineJoin = 'miter'; // miter round bevel
+context.miterLimit = 1;
 
 context.beginPath();
 context.moveTo(100, 100);
 context.lineTo(400, 100);
 context.lineTo(100, 300);
+context.fillStyle = '#00ff00';
+context.strokeStyle = '#ff0000';
+context.closePath();
 
+context.fill(); // context.stroke() context.fill()
 context.stroke(); // context.stroke() context.fill()
+
+context.setLineDash([10, 20, 5]);
+context.beginPath();
+context.moveTo(25,25);
+context.lineTo(205,25);
+context.stroke(); // context.stroke() context.fill()
+
+context.setLineDash([10, 20]);
+context.beginPath();
+context.moveTo(800,50);
+context.lineTo(205,50);
+context.stroke(); // context.stroke() context.fill()
+
